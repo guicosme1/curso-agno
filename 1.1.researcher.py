3d1c3ed22e35d4a709b7e4f91d3ed2324fdaf480
guicosme1 <gui.c.p.dal@gmail.com>
2025-08-19 21:22:22 -0300

    agno testes

---
+++ 1.1.researcher.py
@@ -0,0 +1,14 @@
+from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
+from agno.models.groq import Groq
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[TavilyTools()]
+    )
+
+agent.print_response("Use suas ferramentas para pesquisar temperatura de hoje em São Mateus - ES")
---
+++ 1.2.analista.py
@@ -0,0 +1,15 @@
+from agno.agent import Agent
+from agno.tools.yfinance import YFinanceTools
+from agno.models.groq import Groq
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools()],
+    instructions="Use tabelas para mostrar a informação final. Não inclua nenhum outro texto"
+    )
+
+agent.print_response("Qual a cotação atual da NVIDIA?", stream=True)
